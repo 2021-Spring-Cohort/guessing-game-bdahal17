2b956f62c7112716f459fb439822150cacd6af38
Bishnu <41921803+bdahal17@users.noreply.github.com>
2021-01-22 09:14:05 -0500

    Update GuessingGameApp.java

--- src/main/java/GuessingGameApp.java
+++ src/main/java/GuessingGameApp.java
@@ -4,6 +4,15 @@ public class GuessingGameApp {
         System.out.println("Guessing Game");
         System.out.println("-------------");
         System.out.println("Guess a number between 1 - 10");
+        int bound = 10;
+        Random rand = new Random(bound);
+        Scanner scan = new Scanner(System.in);
+        int scanLine = scan.nextInt();
+        if(scanLine != 10) {
+            System.out.println("Keep guessing);
+        } else if(scanLine == scanLine) {
+            System.out.println("Great! You're correct");
+        }
     }
 
 }
